75a41657add54825a85eff5afdae6ba697ddffa4
chriscarolan <89954045+chriscarolan@users.noreply.github.com>
2024-10-10 15:25:21 -0400

    Finished code for step 2 and 3, included sources of where I got assistance from

--- js_clue.js
+++ js_clue.js
@@ -1,30 +1,48 @@
-
-let suspects = ["Pete Alonso", 'Francisco Lindor', "Brandon Nimmo",
+let suspects = ["Pete Alonso", "Francisco Lindor", "Brandon Nimmo",
     "Mark Vientos", "Jose Quintana", "Edwin Diaz"
 ];
-let weapons = ["Baseball Bat", "Basebal", "Knife", "Gun", "Brass Knuckle",
+let weapons = ["Baseball Bat", "Baseball", "Knife", "Gun", "Brass Knuckles",
     "Throwing Stars", "Sword", "Chainsaw"
 ];
-let room = ["Living Room", "Bathroom", "Dining Room", "Basement", "Murder Room",
+let rooms = ["Living Room", "Bathroom", "Dining Room", "Basement", "Murder Room",
     "Fun Room", "Kitchen", "Happy Room", "Mets Room", "Radio Room"
 ];
 
+//fisher yates sorting algorithm
+// found at https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/
 
-let count = 0;
-let countDisplay = document.getElementById("count");
-let counterButton = document.getElementById("counterButton");
-let resetter = document.getElementById("resetter");
-let decounterButton = document.getElementById("decounterButton");
+const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+    return array;
+}
+// used this to help with set up, had to change a bit
+// https://www.geeksforgeeks.org/how-to-select-a-random-element-from-array-in-javascript/#
+function selectRandom(array) {
+    const randomIndex = Math.floor(Math.random() * array.length); // Get a valid random index
+    const randomCard = array[randomIndex];
+    return randomCard;
+}
 
-counterButton.addEventListener("click", function(e) {
-    count++;
-    countDisplay.textContent = count;
-});
-resetter.addEventListener("click", function(e) {
-    count = 0;
-    countDisplay.textContent = count;
-});
-decounterButton.addEventListener("click", function(e) {
-    count--;
-    countDisplay.textContent = count;
+function pickMystery() {
+    return {
+        suspect: selectRandom(suspects),
+        weapon: selectRandom(weapons),
+        room: selectRandom(rooms)
+    };
+}
+
+function revealMystery(mystery) {
+    return `${mystery.suspect} killed Mr. Marist using the ${mystery.weapon} in the ${mystery.room}!`;
+}
+
+const revealButton = document.getElementById("revealButton");
+const mysteryMessage = document.getElementById("mysteryMessage");
+
+revealButton.addEventListener("click", function() {
+    const mystery = pickMystery();
+    const revealMessage = revealMystery(mystery);
+    mysteryMessage.textContent = revealMessage; 
 });
